e03a7f427aa111c0a78e6d8ac6f7c2006fdbcebb
asherhkhan99 <65026695+asherhkhan99@users.noreply.github.com>
2020-06-27 20:23:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = ""
+}
+
+function getNumber(num){
+        var result = document.getElementById("result");
+        result.value += num
+    }
+    
+
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value) 
+}
+
